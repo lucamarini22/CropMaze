629144d96c0908d5012dcecb3932bf3e582169ca
Manuel Gabrielli <manuel.gabrielli@studio.unibo.it>
2019-04-26 12:35:36 +0200

    add the vertical box in MainMenu

--- src/main/java/it/unibo/oop/bbgmm/Boundary/MainMenu.java
+++ src/main/java/it/unibo/oop/bbgmm/Boundary/MainMenu.java
@@ -1,31 +1,80 @@
 package it.unibo.oop.bbgmm.Boundary;
 
+import it.unibo.oop.bbgmm.Utilities.Resolution;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
 
-import java.awt.*;
 
 public class MainMenu extends Scene {
-    private Toolkit tk = Toolkit.getDefaultToolkit();
-    private Dimension d = tk.getScreenSize();
-    private final double width;
-    private final double height;
+    private static double width;
+    private static double height;
     private static Stage primaryStage;
 
+    Button exitButton = new Button("EXIT");
+    Button newGameButton = new Button("NEW GAME");
+    Button scoreButton = new Button("SCORE");
+    Button resolutionButton = new Button("RESOLUTION");
+
+    private VBox menuBox;
+    private int currentItem = 0;
+
     public MainMenu() {
-        super(new StackPane());
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension d = tk.getScreenSize();
-        this.width = d.width/2;
-        this.height = (2*d.height)/3;
+        super(new AnchorPane());
+        setResolution();
+        this.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP) {
+                if (currentItem > 0) {
+                    getButton(currentItem).setDisable(false);
+                    getButton(--currentItem).setDisable(true);
+                }
+            }
+            if (e.getCode() == KeyCode.DOWN) {
+                if (currentItem < menuBox.getChildren().size() - 1) {
+                    getButton(currentItem).setDisable(false);
+                    getButton(++currentItem).setDisable(true);
+                }
+            }
+            if (e.getCode() == KeyCode.ENTER) {
+                System.out.println("Fare cose");
+                //fa qualcosa in base al bottone quando premo enter
+            }
+        });
+    }
+
+    private void createBox(){
+        menuBox = new VBox(10,
+                newGameButton,
+                scoreButton,
+                resolutionButton,
+                exitButton);
+
+        menuBox.setAlignment(Pos.TOP_CENTER);
+        menuBox.setTranslateX(360);
+        menuBox.setTranslateY(300);
+    }
+
+    private Button getButton(int index) {
+
+        return (Button) menuBox.getChildren().get(index);
 
     }
 
     public static MainMenu getMainMenu(Stage stage){
         primaryStage = stage;
+        setResolution();
         return new MainMenu();
+    }
+
+    private static void setResolution(){
+        height= Resolution.getHeight();
+        width=Resolution.getWidth();
     }
 
 }
--- src/main/java/it/unibo/oop/bbgmm/Boundary/MainWindow.java
+++ src/main/java/it/unibo/oop/bbgmm/Boundary/MainWindow.java
@@ -1,29 +1,23 @@
 package it.unibo.oop.bbgmm.Boundary;
 
+import it.unibo.oop.bbgmm.Utilities.Resolution;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.awt.*;
 
 public class MainWindow extends Application {
-
-    private final double width;
-    private final double height;
     private final Stage primaryStage = new Stage();
 
     public MainWindow() {
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension d = tk.getScreenSize();
-        this.width = d.width/2;
-        this.height = (2*d.height)/3;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage.setTitle("CROP MAZE");
         this.primaryStage.setFullScreen(false);
-        this.primaryStage.setWidth(this.width);
-        this.primaryStage.setHeight(this.height);
+        this.primaryStage.setWidth(Resolution.getWidth());
+        this.primaryStage.setHeight(Resolution.getHeight());
         this.primaryStage.centerOnScreen();
 
         this.primaryStage.setOnCloseRequest(e -> {
