a5a2826c257999813dd00094dc439531a760d39c
Stefania Bernabè <stefania.bernabe@studio.unibo.it>
2019-05-10 17:10:29 +0200

    modified Movement, AbstractMovement and EntityState

--- src/main/java/it/unibo/oop/bbgmm/Entity/AbstractMovement.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/AbstractMovement.java
@@ -1,4 +1,35 @@
 package it.unibo.oop.bbgmm.Entity;
 
-public class AbstractMovement {
+public abstract class AbstractMovement extends AbstractComponent implements Movement {
+    private State currentState = State.STABLE;
+    private Direction directionMovement = Direction.NOTHING;
+
+
+    /**
+     * Set the new state of the entity
+     * @param newState
+     */
+    protected final void setState(final State newState){
+        if(!currentState.equals(newState)){
+            currentState = newState;
+        }
+    }
+
+    protected final Direction getDesiredDirection(){
+        return directionMovement;
+    }
+
+    protected final void setDesiredDirection(final Direction changeDirection){
+        directionMovement = changeDirection;
+    }
+
+    @Override
+    public void move(Direction direction) {
+
+    }
+
+    @Override
+    public State getState() {
+        return currentState;
+    }
 }
--- src/main/java/it/unibo/oop/bbgmm/Entity/Movement.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Movement.java
@@ -1,4 +1,32 @@
 package it.unibo.oop.bbgmm.Entity;
 
-public interface Movement {
+/**
+ * for manage movement of the entity
+ */
+public interface Movement extends EntityComponent {
+
+    /**
+     *
+     * @param direction
+     *          the direction where the entity will move
+     */
+    void move(Direction direction);
+
+    /**
+     *
+     * @return Movement state
+     */
+    State getState();
+
+
+
+    enum State implements EntityState{
+        STABLE, WALKING, SHOOTING, DYING;
+    }
+
+    enum Direction{
+        NORTH, SOUTH, EAST, WEST, NOTHING;
+    }
+
+
 }
