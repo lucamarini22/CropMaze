8e338b66a55168c1b78867c71115ed9f195757e5
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-05-11 15:42:03 +0200

    Changes to EntityBody, deleted some methods

--- src/main/java/it/unibo/oop/bbgmm/Entity/Component/EntityBody.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Component/EntityBody.java
@@ -1,34 +1,32 @@
 package it.unibo.oop.bbgmm.Entity.Component;
 
 import it.unibo.oop.bbgmm.Utilities.Pair;
-import java.util.stream.Stream;
+
 
 public interface EntityBody extends EntityComponent{
 
     /**
-     * @return the position
+     * @return The position
      */
     Pair<Integer, Integer> getPosition();
 
     /**
      *
-     * @param speed
-     *          the velocity of the entity
+     * @param velocity
+     *          The speed of the entity
      */
-    void setVelocity(int speed);
-
-    int getVelocity();
-
-    /**
-     * get all {@link EntityBody} object colliding with this one
-     * @return a Stream of the object that colliding with the entity
-     */
-    Stream<EntityBody> getContacts();
+    void setVelocity(double velocity);
 
     /**
      *
-     * @return true if can't cross the body because it's not solid
+     * @return Current speed
      */
-    boolean notGoThrough();
+    double getVelocity();
+
+    /**
+     *
+     * @return Body dimension
+     */
+    Pair<Double, Double> getDimension();
 
 }
