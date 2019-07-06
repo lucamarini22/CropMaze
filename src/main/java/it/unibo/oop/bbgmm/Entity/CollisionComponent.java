f738480e32f2a603435630c60a3d304dc31258b7
Luca <luca.bombardi4@studio.unibo.it>
2019-07-06 17:15:51 +0200

    created class CollisonComponent

---
+++ src/main/java/it/unibo/oop/bbgmm/Entity/CollisionComponent.java
@@ -0,0 +1,41 @@
+package it.unibo.oop.bbgmm.Entity;
+
+import java.awt.*;
+
+public class CollisionComponent extends AbstractEntityComponent implements Collidable {
+
+    private final CollisionLabel label;
+    private final EventSource<Collision> collisionEvent;
+    private final Rectangle shape;
+
+    public CollisionComponent(Entity owner, final Rectangle shape, final CollisionLabel label){
+
+        this.shape = shape;
+        this.label = label;
+        this.collisionEvent = new EventSource<Collision>();
+    }
+    @Override
+    public Rectangle getShape() {
+        return this.shape;
+    }
+
+    @Override
+    public Pair<Double, Double> getPosition() {
+        return null;
+    }
+
+    @Override
+    public CollisionLabel getCollisionLabel() {
+        return this.label;
+    }
+
+    @Override
+    public void notifyCollision(Collision collision) {
+        this.collisionEvent.trigger(collision);
+    }
+
+    @Override
+    public Event<Collision> getEvent() {
+        return this.collisionEvent;
+    }
+}
