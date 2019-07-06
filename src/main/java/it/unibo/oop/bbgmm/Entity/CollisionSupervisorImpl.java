f9dc3b9e9dfe753875079b8f6d45a63a83b8f2c1
Luca <luca.bombardi4@studio.unibo.it>
2019-07-06 17:16:44 +0200

    created class CollisionSupervisorImpl

---
+++ src/main/java/it/unibo/oop/bbgmm/Entity/CollisionSupervisorImpl.java
@@ -0,0 +1,44 @@
+package it.unibo.oop.bbgmm.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollisionSupervisorImpl implements CollisonSupervisor {
+
+    private final List<Collidable> collidableComponents;
+
+    public CollisionSupervisorImpl(){
+        this.collidableComponents = new ArrayList<>();
+    }
+
+
+    @Override
+    public void searchCollision() {
+        for(int i = 0; i < this.collidableComponents.size(); i++){
+            for (int j = i + 1; j< this.collidableComponents.size(); j++){
+                final Collidable collidable1 = collidableComponents.get(i);
+                final Collidable collidable2 = collidableComponents.get(j);
+                verifyCollision(collidable1, collidable2);
+            }
+        }
+    }
+
+    @Override
+    public void addCollidableComponent(Collidable collidable) {
+        this.collidableComponents.add(collidable);
+    }
+
+    @Override
+    public void removeCollidableComponent(Collidable collidable) {
+        this.collidableComponents.remove(collidable);
+    }
+
+    private void verifyCollision(final Collidable collidable1, final Collidable collidable2){
+        if(collidable1.getCollisionLabel().canCollideWith(collidable2.getCollisionLabel())){
+            if(collidable1.getShape().intersects(collidable2.getShape())) {
+                collidable1.notifyCollision(new Collision(collidable2));
+                collidable2.notifyCollision((new Collision(collidable1)));
+            }
+        }
+    }
+}
