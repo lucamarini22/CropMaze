2da05b1796ecfa73707a57a271014a83b814147a
Luca Marini <luca.marini9@studio.unibo.it>
2019-07-15 13:09:24 +0200

    create GameFieldImpl

---
+++ src/main/java/it/unibo/oop/bbgmm/Entity/GameFieldImpl.java
@@ -0,0 +1,29 @@
+package it.unibo.oop.bbgmm.Entity;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Runs the game field.
+ */
+public class GameFieldImpl implements GameField {
+
+
+    private final Set<Entity> entities = new LinkedHashSet<>();
+
+    @Override
+    public void update(final double up) {
+        //incompleted
+        this.entities.stream()
+                     .forEach(e -> e.update(up));
+    }
+
+    @Override
+    public void addEntity(final Entity entity) {
+        entities.add(entity);
+    }
+
+    private void removeEntity(final Entity entity) {
+        this.entities.remove(entity);
+    }
+}
