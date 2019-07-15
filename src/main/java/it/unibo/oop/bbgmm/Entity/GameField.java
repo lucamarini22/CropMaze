76ecc724d17c096cce8e92fcc6cb0c37dcb652ad
Luca Marini <luca.marini9@studio.unibo.it>
2019-07-15 13:12:47 +0200

    add method addEntity

--- src/main/java/it/unibo/oop/bbgmm/Entity/GameField.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/GameField.java
@@ -1,15 +1,21 @@
 package it.unibo.oop.bbgmm.Entity;
 
 /**
- * Represent the game field.
+ * Runs the game field.
  */
 public interface GameField {
 
     /**
      * Update the field simulation.
      *
-     * @param up
-     *            Time to simulate, in seconds.
+     * @param up Time to simulate, in seconds
      */
     void update(double up);
+
+    /**
+     *
+     * @param entity
+     *      The entity that is going to be added in the game field
+     */
+    void addEntity(Entity entity);
 }
