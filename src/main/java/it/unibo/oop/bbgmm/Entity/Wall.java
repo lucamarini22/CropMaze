bef26bfa8bb8c2dba8658b8b00dd06355323a1fe
Luca Marini <luca.marini9@studio.unibo.it>
2019-07-15 13:12:02 +0200

    change constructor in Wall class

--- src/main/java/it/unibo/oop/bbgmm/Entity/Wall.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Wall.java
@@ -1,13 +1,29 @@
 package it.unibo.oop.bbgmm.Entity;
 
+import it.unibo.oop.bbgmm.Entity.Component.BodyBuilder;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+
+/**
+ * Represent a wall.
+ */
 public class Wall extends AbstractEntity {
 
+    private static final double WIDTH = 1.1;
+    private static final double HEIGHT = 1.2;
+    private static final Dimension2D SIZE = new Dimension2D(WIDTH, HEIGHT);
+
     /**
-     * constructor for Abstract Entity
      *
-     * @param body
+     * @param bodyBuilder
+     *      The bodybuilder of the wall
+     * @param position
+     *      The position of the wall
      */
-    public Wall(EntityBody body) {
-        super(body);
+    public Wall(final BodyBuilder bodyBuilder, final Point2D position) {
+        super(bodyBuilder
+                .setPosition(position)
+                .setDimension(SIZE)
+                .build());
     }
 }
