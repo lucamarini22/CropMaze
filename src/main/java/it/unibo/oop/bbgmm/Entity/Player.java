b9ec7232693bee88e021288a8623d16836aa05b3
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-07-16 12:01:46 +0200

    Modify Player

--- src/main/java/it/unibo/oop/bbgmm/Entity/Player.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Player.java
@@ -1,9 +1,12 @@
 package it.unibo.oop.bbgmm.Entity;
 
 
+import it.unibo.oop.bbgmm.Entity.Collision.CollisionLabel;
 import it.unibo.oop.bbgmm.Entity.Component.*;
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
+
+import java.awt.*;
 
 public class Player extends AbstractEntity {
     private static final Dimension2D SIZE = new Dimension2D(1.1,1.2);
@@ -13,10 +16,14 @@ public class Player extends AbstractEntity {
         super(bodyBuilder
                 .setPosition(position)
                 .setDimension(SIZE)
+                .setDirection(Direction.NOTHING)
+                .setMovable(true)
                 .build());
         add(new LifeComponent(health));
         add(new Feet(WALKING_SPEED));
         add(new WeaponImpl(weapon));
-
+        add(new CollisionComponent(this,new Rectangle(), CollisionLabel.PLAYER));
     }
+
 }
+
