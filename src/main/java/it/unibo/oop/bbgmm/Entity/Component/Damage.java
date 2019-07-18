c2aee6f0ba7f2483861378bcd3ab24ba04178317
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-07-18 13:22:49 +0200

    Fix Damage interface

--- src/main/java/it/unibo/oop/bbgmm/Entity/Component/Damage.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Component/Damage.java
@@ -1,4 +1,19 @@
 package it.unibo.oop.bbgmm.Entity.Component;
 
-public interface Damage {
+/**
+ * Manage the entity (alien, bullet) damage
+ */
+public interface Damage extends EntityComponent{
+    /**
+     *
+     * @return  The current amount of damage
+     */
+    double getDamage();
+
+    /**
+     *
+     * @param damage
+     *      New damage dealt by the entity
+     */
+    void setDamage(double damage);
 }
