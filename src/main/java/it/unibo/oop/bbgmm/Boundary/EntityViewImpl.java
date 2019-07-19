9dba2c267228bc426b3dabab09ea58b129d4d11a
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-07-19 16:47:53 +0200

    Create EntityViewImpl

---
+++ src/main/java/it/unibo/oop/bbgmm/Boundary/EntityViewImpl.java
@@ -0,0 +1,57 @@
+package it.unibo.oop.bbgmm.Boundary;
+
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
+
+public class EntityViewImpl implements EntityView {
+    private final ImageView image = new ImageView();
+    private final Dimension2D dimension;
+    private final Group myGroup;
+
+    public EntityViewImpl(final Group group, final Dimension2D dimension){
+        this.myGroup = group;
+        this.dimension = dimension;
+
+        this.myGroup.getChildren().add(image);
+    }
+
+    @Override
+    public void setPosition(Point2D newPosition) {
+        this.image.setTranslateX(newPosition.getX() - this.image.getBoundsInLocal().getWidth() / 2);
+        this.image.setTranslateY(newPosition.getY() - this.image.getBoundsInLocal().getHeight() / 2);
+    }
+
+    @Override
+    public void setDimension(Dimension2D dimension) {
+        this.image.setFitHeight(dimension.getHeight());
+        this.image.setFitWidth(dimension.getHeight());
+    }
+
+    @Override
+    public Point2D getPosition() {
+        return new Point2D(this.image.getTranslateX(),image.getTranslateY());
+    }
+
+    @Override
+    public void removeFromView() {
+        this.myGroup.getChildren().remove(this.image);
+    }
+
+    @Override
+    public ImageView getView() {
+        return this.image;
+    }
+
+    @Override
+    public Dimension2D getDimension() {
+        return this.dimension;
+    }
+
+    @Override
+    public Group getGroup() {
+        return this.myGroup;
+    }
+}
