21a2d8fd8986dbce04631c3b4071c14f0e4abd5c
Manuel Gabrielli <manuel.gabrielli@studio.unibo.it>
2019-07-30 10:54:00 +0200

    Resolved merge conflict in /LifelessEntityController

--- src/main/java/it/unibo/oop/bbgmm/Control/LifelessEntityController.java
+++ src/main/java/it/unibo/oop/bbgmm/Control/LifelessEntityController.java
@@ -4,26 +4,11 @@ import it.unibo.oop.bbgmm.Boundary.LifelessEntityView;
 import it.unibo.oop.bbgmm.Entity.Entity;
 
 
-<<<<<<< HEAD
-public class LifelessEntityController extends AbstractLifelessEntityController<LifelessEntityView> {
-=======
 /**
  * Base class for AbstractLifeEntityController
  */
 public class LifelessEntityController extends AbstractLifelessEntityController<LifelessEntityView> {
-    /**
-     *
-     * @param entity
-     *             the object to control
-     * @param entityView
-     *              the object to update
-     */
-    public LifelessEntityController(final Entity entity, final LifelessEntityView entityView) {
-        super(entity, entityView);
-    }
-
->>>>>>> 02dff5e65a74fd2e8fe0e3398b05f9ab92282505
-
+    
         /**
          * @param entity
          *            The {@link Entity} object to control.
