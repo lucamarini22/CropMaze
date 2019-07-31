42e17485d1314fa37b98d17df8ccef6ad5d223ef
Luca Marini <luca.marini9@studio.unibo.it>
2019-07-31 15:21:38 +0200

    create and implement interface and class for a factory of the views of the entities

---
+++ src/main/java/it/unibo/oop/bbgmm/Boundary/EntityViewFactory.java
@@ -0,0 +1,15 @@
+package it.unibo.oop.bbgmm.Boundary;
+
+/**
+ * Factory for the views of the entities.
+ */
+public interface EntityViewFactory {
+
+    PlayerView createPlayerView();
+
+    //AlienView createAlienView();
+
+    //CoinView createCoinView();
+
+    //upgrades and power ups
+}
