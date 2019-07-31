b3782a6eaf923ed137360623527d90e860ee0d7d
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-07-31 11:20:10 +0200

    Add private methods that check possible wall collision

--- src/main/java/it/unibo/oop/bbgmm/Entity/Component/Feet.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/Component/Feet.java
@@ -3,6 +3,11 @@ package it.unibo.oop.bbgmm.Entity.Component;
 import it.unibo.oop.bbgmm.Entity.AbstractMovement;
 import it.unibo.oop.bbgmm.Entity.Direction;
 import it.unibo.oop.bbgmm.Entity.Entity;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+
+import java.util.Set;
 
 /**
  * Permit at the entity to walk
@@ -10,14 +15,18 @@ import it.unibo.oop.bbgmm.Entity.Entity;
 public class Feet extends AbstractMovement {
 
     private final double walkingSpeed;
+    private final Set<Entity> walls;
 
     /**
      *
      * @param walkingSpeed
-     *            entity speed for the movement
+     *      entity speed for the movement
+     * @param walls
+     *      walls for checking collisions
      */
-    public Feet(final double walkingSpeed){
+    public Feet(final double walkingSpeed, final Set<Entity> walls){
         super();
+        this.walls = walls;
         this.walkingSpeed = walkingSpeed;
     }
 
@@ -35,9 +44,46 @@ public class Feet extends AbstractMovement {
 
     }
 
+    /**
+     * Util method that check wall collision and stop the movement
+     * @param distanceVector
+     *      The distance vector
+     * @return
+     *      True if the entity can move
+     *
+     *
+     */
+    private boolean wallChecker(final Point2D distanceVector){
+        Point2D newDistanceVector = this.calculatePosition(distanceVector);
+        if(getOwner().isPresent()){
+            Dimension2D dimension  = getOwner().get().getBody().getDimension();
+            Rectangle2D shape = new Rectangle2D(newDistanceVector.getX(),newDistanceVector.getY(),
+                                                dimension.getWidth(),dimension.getHeight());
+            return this.walls.stream().anyMatch(w -> w.getBody().getShape().intersects(shape));
+        }
+        return false;
+    }
+
+    /**
+     * Calculate new distance vector based on the speed
+     * @param distanceVector
+     *      The distance vector
+     * @return
+     *      The new distance vector
+     */
+    private Point2D calculatePosition(final Point2D distanceVector){
+        final double hf = distanceVector.getX() > 0 ? 1 : distanceVector.getX() < 0 ? -1 : 0;
+        final double vf = distanceVector.getY() > 0 ? 1 : distanceVector.getY() < 0 ? -1 : 0;
+        return new Point2D(hf * walkingSpeed, vf * walkingSpeed);
+    }
 
     @Override
-    public void move(Direction direction, double speed) {
+    public void move( Direction direction, double speed) {
+        //utilizzando questo distancevector dovrai modificare la posizione dell'entità
+        //ovviamente dovrai modificare il metodo e fare in modo che chieda in input un point2d
+        //if(wallChecker(distanceVector)){
+        //    Point2D newDistanceVector = this.calculatePosition(distanceVector);
+        //}
         setDesiredDirection(direction);
         super.move(direction, speed);
     }
