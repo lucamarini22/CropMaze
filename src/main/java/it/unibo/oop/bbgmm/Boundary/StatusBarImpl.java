c1f0a9f5fc9e7c56d26ca4170ef498ad9feef800
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2019-08-02 14:25:42 +0200

    Add images for the status bar

--- src/main/java/it/unibo/oop/bbgmm/Boundary/StatusBarImpl.java
+++ src/main/java/it/unibo/oop/bbgmm/Boundary/StatusBarImpl.java
@@ -1,27 +1,29 @@
 package it.unibo.oop.bbgmm.Boundary;
 
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 
-import java.awt.*;
+
 
 public class StatusBarImpl implements StatusBar {
 
-    private Graphics maxHealthBar;
-    private Graphics currentHealthBar;
+    private static final int PADDING = 20;
+    private static final Image maxHealthBar = new Image("images/damageBar.png");
+    private static final Image currentHealthBar = new Image("images/lifeBar.png");
 
 
     public StatusBarImpl(){
-        maxHealthBar.setColor(Color.red);
-        currentHealthBar.setColor(Color.green);
+
     }
     @Override
     public void setMaxLifePoints(int maxLifePoints) {
-        maxHealthBar.drawRect(0,0, maxLifePoints,0);
+
     }
 
     @Override
     public void setCurrentLifePoints(int currentLifePoints) {
-        currentHealthBar.drawRect(0,0, currentLifePoints,0);
+
     }
 
     @Override
