94bf2f6adf7641b9428167a35a14171f644c27ba
Luca Marini <luca.marini9@studio.unibo.it>
2019-08-03 14:33:44 +0200

    modify TiledMap so now walls spawns before the entities

--- src/main/java/it/unibo/oop/bbgmm/Control/LevelImpl.java
+++ src/main/java/it/unibo/oop/bbgmm/Control/LevelImpl.java
@@ -129,7 +129,12 @@ public final class LevelImpl implements Level {
 
     private void loadEntityObjects(final ObjectGroup layer) {
         layer.forEach(mapObj -> {
-            final Point2D position = invertY(new Point2D(mapObj.getX() / POSITION_DIVISOR_SPAWN_X, mapObj.getY() / POSITION_DIVISOR_SPAWN_Y));
+            final Point2D position = invertY(new Point2D(45, 70));
+                this.gameField.getWalls().forEach(w -> System.out.println(w.getBody().getPosition()));
+            this.gameField.getWalls().forEach(w -> System.out.println(w.getBody().getDimension()));
+
+            System.out.println("pos=" + position);
+
             final String type = mapObj.getType();
             Entity entity;
             switch (EntityType.valueOf(type)) {
