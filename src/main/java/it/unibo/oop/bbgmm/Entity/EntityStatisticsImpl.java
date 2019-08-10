d2cc6729930a624531002d588ee0ac78048230f0
Manuel Gabrielli <manuel.gabrielli@studio.unibo.it>
2019-08-10 12:28:57 +0200

    Solved problem with coin image

--- src/main/java/it/unibo/oop/bbgmm/Entity/EntityStatisticsImpl.java
+++ src/main/java/it/unibo/oop/bbgmm/Entity/EntityStatisticsImpl.java
@@ -6,7 +6,7 @@ package it.unibo.oop.bbgmm.Entity;
 public final class EntityStatisticsImpl implements EntityStatistics {
 
     private static final int COIN_VALUE = 10;
-    private static final int PLAYER_HEALTH = 10;
+    private static final int PLAYER_HEALTH = 10000;
     private static final int ENEMY_HEALTH = 10;
 
     @Override
