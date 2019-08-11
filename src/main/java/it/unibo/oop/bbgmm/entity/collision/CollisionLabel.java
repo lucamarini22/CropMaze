d2b1e98fbcb4fb567531a8a70c48fbebde4487b6
Luca <luca.bombardi4@studio.unibo.it>
2019-08-11 15:06:32 +0200

    checkstyle and pmd in entity classes

--- src/main/java/it/unibo/oop/bbgmm/entity/collision/CollisionLabel.java
+++ src/main/java/it/unibo/oop/bbgmm/entity/collision/CollisionLabel.java
@@ -1,42 +1,76 @@
 package it.unibo.oop.bbgmm.entity.collision;
 
+/**
+ * It is responsible to decide if an object type can collide with another.
+ */
 public enum CollisionLabel {
 
+    /**
+     * Collision tag for the player object.
+     */
     PLAYER {
+        @Override
         public boolean canCollideWith(final CollisionLabel other) {
             return other == ALIEN || other == COIN || other == POWER;
         }
     },
 
+    /**
+     * Collision tag for the aline object.
+     */
     ALIEN {
-        public boolean canCollideWith(final CollisionLabel other){
+        @Override
+        public boolean canCollideWith(final CollisionLabel other) {
             return other == SHOT;
         }
     },
 
+    /**
+     * Collision tag for the shot object.
+     */
     SHOT {
-        public boolean canCollideWith(final CollisionLabel other){
+        @Override
+        public boolean canCollideWith(final CollisionLabel other) {
             return other == ALIEN || other == WALL;
         }
     },
 
+    /**
+     * Collision tag for the power object.
+     */
     POWER {
-        public boolean canCollideWith(final CollisionLabel other){
-            return other == PLAYER;}
+        @Override
+        public boolean canCollideWith(final CollisionLabel other) {
+            return other == PLAYER; }
     },
 
-    COIN{
-        public boolean canCollideWith(final CollisionLabel other){
+    /**
+     * Collision tag for the coin object.
+     */
+    COIN {
+        @Override
+        public boolean canCollideWith(final CollisionLabel other) {
             return other == PLAYER;
         }
     },
 
-    WALL{
+    /**
+     * Collision tag for the wall object.
+     */
+    WALL {
+        @Override
         public boolean canCollideWith(final CollisionLabel other) {
-            return other == SHOT;}
+            return other == SHOT; }
     };
 
-    public boolean canCollideWith(final CollisionLabel other){
+    /**
+     * Returns true if objects can collide with objects of the given one type.
+     * @param other
+     *      the other type.
+     * @return
+     *      true if the objects can collide.
+     */
+    public boolean canCollideWith(final CollisionLabel other) {
         return true;
     }
 
